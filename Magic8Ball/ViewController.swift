53febe93c7d500158c1c75f99e7f9811afb7a489
Dionte <Dionte_sb@yahoo.com>
2019-08-24 19:06:51 -0700

    Added in the function to generate the random 8 ball answer.

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var askQuestionText: UITextField!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    // array to hold the names of the random 8 ball images.
+    let ballImageNames = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        askQuestionText.delegate = self
+        
     }
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        questionLabel.text = textField.text
+    }
+    
+    // Have the random 8 ball display an answer.
+    func shakeMagicBall() {
+        
+        let randomNumber = Int.random(in: 0..<5)
+        
+        imageView.image = UIImage(named: ballImageNames[randomNumber])
+    }
+    
+    @IBAction func tapButtonTapped(_ sender: UIButton) {
+        shakeMagicBall()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        shakeMagicBall()
+    }
+    
 
 }
 
